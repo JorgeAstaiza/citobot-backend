1955f40f278585a6686787b9326a3a588c2e8b06
Jorge Astaiza <jorge.astaiza@sailpoint.com>
2022-09-30 16:05:54 -0500

    problema de cors

--- models/server.js
+++ models/server.js
@@ -52,14 +52,14 @@ class Server {
 	config() {
 		this.app.set('port', this.port); //defino el puerto del servidor
 		this.app.use(morgan('dev')); //para poder ver las peticiones por consola
-		this.app.use(cors());
+		this.app.use(cors({ origin: 'http://http://localhost/:8888' }));
 		this.app.use(express.json({ limit: '50000mb' }));
 		this.app.use(express.urlencoded({ limit: '50000mb', extended: true, parameterLimit: 5000000000000 }));
 		this.app.use('/api-doc', swaggerUI.serve, swaggerUI.setup(swaggerJsDoc(swaggerSpec)));
 		// Add headers before the routes are defined
 		this.app.use(function (req, res, next) {
 			// Website you wish to allow to connect
-			res.setHeader('Access-Control-Allow-Origin', '*');
+			res.setHeader('Access-Control-Allow-Origin', 'http://http://localhost/:8888');
 
 			// Request methods you wish to allow
 			res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
@@ -70,6 +70,9 @@ class Server {
 			// Set to true if you need the website to include cookies in the requests sent
 			// to the API (e.g. in case you use sessions)
 			res.setHeader('Access-Control-Allow-Credentials', true);
+
+			// Pass to next layer of middleware
+			next();
 		});
 	}
 	routes() {
