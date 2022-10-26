e6ad0b2607dddc5a94c78cb461afa5093c62cca6
Jorge Astaiza <jorge.astaiza@sailpoint.com>
2022-10-25 19:08:08 -0500

    cambio puerto dinamico

--- models/server.js
+++ models/server.js
@@ -28,7 +28,7 @@ const swaggerSpec = {
 class Server {
 	constructor() {
 		this.app = express();
-		this.port = 8080;
+		this.port = process.env.PORT || 8080;
 		this.personasPath = '/api/personas';
 		this.usuarioPath = '/api/usuarios';
 		this.pacientePath = '/api/pacientes';
