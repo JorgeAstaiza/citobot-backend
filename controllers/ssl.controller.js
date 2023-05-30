5e853b2980a3adf67c174df728702be6f1fb470d
Jorge Astaiza <jorge.astaiza@sailpoint.com>
2023-05-30 14:52:48 -0500

    nuevo pki

--- controllers/ssl.controller.js
+++ controllers/ssl.controller.js
@@ -1,5 +1,5 @@
 const getSSL = (req = request, res = response) => {
-	res.sendFile('/home/ubuntu/citobot-backend/.well-known/2A6C7761D4D4C8DAFD0E56D4048937B4.txt');
+	res.sendFile('/home/ubuntu/citobot-backend/.well-known/pki-validation/2A6C7761D4D4C8DAFD0E56D4048937B4.txt');
 };
 module.exports = {
 	getSSL
