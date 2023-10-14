c435a535558ac2e13ed032f1fec0b783b8ab4d08
Jorge Astaiza <alexandercordoba.dev@gmail.com>
2023-10-14 17:42:02 -0500

    nuevo ssl backend http://18.117.240.91/

--- controllers/ssl.controller.js
+++ controllers/ssl.controller.js
@@ -1,5 +1,5 @@
 const getSSL = (req = request, res = response) => {
-	res.sendFile('/home/ubuntu/citobot-backend/.well-known/pki-validation/3C354193A6BA8D8627B16431AA041FB2.txt');
+	res.sendFile('/home/ubuntu/citobot-backend/.well-known/pki-validation/E47563CD58A1705D29BB63CBE2714737.txt');
 };
 module.exports = {
 	getSSL
--- routes/ssl.routes.js
+++ routes/ssl.routes.js
@@ -2,6 +2,6 @@ const { Router } = require('express');
 const { getSSL } = require('../controllers/ssl.controller');
 const router = Router();
 
-router.get('/.well-known/pki-validation/3C354193A6BA8D8627B16431AA041FB2.txt', getSSL);
+router.get('/.well-known/pki-validation/E47563CD58A1705D29BB63CBE2714737.txt', getSSL);
 
 module.exports = router;
